d60b7933b628c3ec98ed12e7979473ab40f89648
Potsawat Chaliewkumnuan <atomer@MacBook-Pro-khxng-Potsawat.local>
2021-06-24 11:16:26 +0700

    search hashtag & content in one API

--- functions/index.js
+++ functions/index.js
@@ -4,13 +4,13 @@ const postService = require('./services/post')
 const config = functions.config()
 
 exports.hashtag = functions.region('asia-southeast2').https.onRequest(async (request, response) => {
-    const { hashtagName, size, page } = request.query
+    const { hashtagName, size, page, content } = request.query
     let pageNumber = 1, sizeNumber = 5
     if (page) pageNumber = Number(page)
     if (size) sizeNumber = Number(size)
 
-    if (!hashtagName) return response.json({ success: false, message: 'hashtagName not found' })
-    const resElastic = await postService.getPostListByHashtag(hashtagName, pageNumber, sizeNumber)
+    if (!hashtagName && !content) return response.json({ success: false, message: 'hashtagName or content search not found' })
+    const resElastic = await postService.getPostList(hashtagName, pageNumber, sizeNumber, content)
     const { body: { hits } } = resElastic
     const postList = hits.hits.map(v => (v._id))
     const formatOut = {
@@ -22,28 +22,6 @@ exports.hashtag = functions.region('asia-southeast2').https.onRequest(async (req
     return response.json({ success: true, data: formatOut })
 
 });
-
-exports.content = functions.region('asia-southeast2').https.onRequest(async (request, response) => {
-    const { search, size, page } = request.query
-    console.log('size',size)
-    let pageNumber = 1, sizeNumber = 5
-    if (page) pageNumber = Number(page)
-    if (size) sizeNumber = Number(size)
-
-    if (!search) return response.json({ success: false, message: 'search not found' })
-    const resElastic = await postService.getPostListByContent(search, pageNumber, sizeNumber)
-    const { body: { hits } } = resElastic
-    const postList = hits.hits.map(v => (v._id))
-    const formatOut = {
-        totalIndex: hits.total.value,
-        totalPage: Math.ceil(hits.total.value / sizeNumber),
-        currentPage: pageNumber,
-        posts: postList,
-    }
-    return response.json({ success: true, data: formatOut })
-
-});
-
 
 exports.ascEvent = functions.region('asia-southeast2').pubsub.topic(config.env.topic).onPublish(async (message, context) => {
 
--- functions/services/post.js
+++ functions/services/post.js
@@ -5,12 +5,12 @@ const findHashtag = require('./findHashtag')
 async function didCreate(eventData) {
     const { posts } = eventData
     const { postId, postedUserId, data, dataType, createdAt, updatedAt, isDeleted } = posts[0]
-   if (dataType !== 'text') {
+    if (dataType !== 'text') {
         throw new Error(`cannot create postType ${dataType} to elastic`)
     }
     const hashTagList = await findHashtag(data.text)
     if (!hashTagList) { return false }
-   
+
     const created = await client.index({
         index: 'post',
         id: postId,
@@ -39,33 +39,26 @@ async function didDelete(postId) {
     return deleted
 }
 
-async function getPostListByHashtag(hashtag, page, size) {
-    const postList = await client.search({
-        index: 'post',
-        body: {
-            from: (page - 1) * size,
-            size,
-            query: {
-                match: { hashtag }
-            },
-            sort: [
-                { updatedAt: 'desc' },
-                { _id: 'asc' }
-            ]
+async function getPostList(hashtag, page, size, content) {
+    let query = {}
+    if (hashtag && content) {
+        query = {
+            bool: {
+                must: []
+            }
         }
-    })
-    return postList
-}
+        if (hashtag) query.bool.must.push({ match: { hashtag } })
+        if (hashtag) query.bool.must.push({ wildcard: { content: `*${content}*` } })
+    }
+    else if (hashtag) query.match = { hashtag }
+    else if (content) query.wildcard = { content: `*${content}*` }
 
-async function getPostListByContent(search, page, size) {
     const postList = await client.search({
         index: 'post',
         body: {
             from: (page - 1) * size,
             size,
-            query: {
-                wildcard: { content:search }
-            },
+            query,
             sort: [
                 { updatedAt: 'desc' },
                 { _id: 'asc' }
@@ -76,5 +69,5 @@ async function getPostListByContent(search, page, size) {
 }
 
 module.exports = {
-    didCreate, didDelete, getPostListByHashtag,getPostListByContent
+    didCreate, didDelete, getPostList
 }
